cb9826e7cdece9e1626e633b99ad25bded4c70de
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-07-29 12:36:43 -0400

    Nuget stuff

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -44,7 +44,7 @@ using System.Runtime.InteropServices;
 #endif
 
 [assembly: AssemblyTitle("SmartIrc4net")]
-[assembly: AssemblyDescription("IRC library for CLI")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("qNETp")]
 [assembly: AssemblyProduct("SmartIrc4net")]
@@ -60,7 +60,7 @@ using System.Runtime.InteropServices;
 // numbers with the '*' character (the default):
 
 [assembly: AssemblyVersion("0.4.5.0")]
-[assembly: AssemblyInformationalVersion("1.0")]
+[assembly: AssemblyInformationalVersion("0.4.6")]
 
 // The following attributes specify the key for the sign of your assembly. See the
 // .NET Framework documentation for more information about signing.
