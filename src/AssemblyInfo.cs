a0e38ace4c5a2aa046cdc9a6ebf8d8f8b0e8c384
Jordan Dominion <Cyberboss@users.noreply.github.com>
2024-11-16 15:14:54 -0500

    Version bump

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -59,8 +59,8 @@ using System.Runtime.InteropServices;
 // You can specify all values by your own or you can build default build and revision
 // numbers with the '*' character (the default):
 
-[assembly: AssemblyVersion("0.4.6.0")]
-[assembly: AssemblyInformationalVersion("0.4.6")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
 
 // The following attributes specify the key for the sign of your assembly. See the
 // .NET Framework documentation for more information about signing.
